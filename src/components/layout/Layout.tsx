d39a210bc4784b66abf4664af189f118e2e9583f
soheekimdev <soheekim.dev@gmail.com>
2024-12-05 23:15:31 +0900

    Chore: clsx 라이브러리 설치, tailwind.config에 fontFamily 추가, Layout 관련 css 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="flex flex-col gap-20 min-h-screen font-Jua bg-brand-blue">
-      <main className="flex-grow">
+      <main className="flex-grow flex items-center justify-center">
         <Outlet />
       </main>
     </div>
