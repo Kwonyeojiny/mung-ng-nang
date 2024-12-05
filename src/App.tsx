9201ac4ad3e45aec698e39f57e24ca83a10f324d
soheekimdev <soheekim.dev@gmail.com>
2024-12-05 20:02:20 +0900

    Chore: 프로젝트 폴더 구조 변경 및 기본 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
-    <>
-      멍는거냥
-    </>
-  )
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
