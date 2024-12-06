56d3186d4089da3c032470511abb577676302250
Kwonyeojiny <kangagi0009@naver.com>
2024-12-06 20:00:22 +0900

    Feat: Navbar 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
     <div className="flex flex-col gap-20 min-h-screen font-Jua bg-brand-blue">
+      <Navbar />
       <main className="flex-grow flex items-center justify-center">
         <Outlet />
       </main>
