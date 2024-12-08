a5a2675687e2d9087665e08d50070b6ce5d8c45d
Kwonyeojiny <kangagi0009@naver.com>
2024-12-09 01:00:42 +0900

    Feat: Home에 들어갈 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import HomeMyPet from '../components/Home/HomeMyPet';
+import Introduce from '../components/Home/Introduce';
+import PetChart from '../components/Home/PetChart';
+import TryCalculator from '../components/Home/TryCalculator';
+
 const Home = () => {
   return (
-    <>
-      Home
-    </>
-  );
-}
+    <div className="flex flex-col gap-8">
+      <Introduce />
+      <TryCalculator />
 
-export default Home
+      {/* 로그인시 */}
+      <HomeMyPet />
+      <PetChart />
+    </div>
+  );
+};
+
+export default Home;
