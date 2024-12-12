3777f4bc749892b914f539d08111368c7dd8d32b
Kwonyeojiny <kangagi0009@naver.com>
2024-12-13 00:46:15 +0900

    Feat: AddPetCard 클릭시 반려동물추가 페이지 이동 Link 추가(주석처리해둠)

--- src/components/Home/HomeMyPet.tsx
+++ src/components/Home/HomeMyPet.tsx
@@ -1,9 +1,11 @@
+// import Link  from 'react-router-dom';
 import Button from '../ui/Button';
 import Card from '../ui/Card';
-import AddPetCard from './AddPetCard';
+// import AddPetCard from './AddPetCard';
 import PetCard from './PetCard';
 
 const HomeMyPet = () => {
+
   return (
     <div className="flex justify-center items-center">
       <Card className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-between">
@@ -17,7 +19,10 @@ const HomeMyPet = () => {
           <Button size="lg">버튼</Button>
         </div>
         <div className="flex gap-16 md:gap-8 justify-center md:justify-end">
-          <AddPetCard />
+          {/* 펫 정보 없을때 사용 */}
+          {/* <Link to='/mypage/new-pet'>
+            <AddPetCard />
+          </Link> */}
           <PetCard />
           <PetCard />
         </div>
